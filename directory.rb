3c2133aa8ab1591b2dc856d825166d929139f357
Thomas Drayton <tomadrayton@gmail.com>
2017-03-29 11:38:28 +0100

    load studnets, step 12 complete

--- directory.rb
+++ directory.rb
@@ -1,5 +1,26 @@
 @students = [] # an empty array accessible to all methods
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -25,6 +46,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -39,6 +62,11 @@ def process(selection)
   when "1"
     input_students
   when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
     show_students
   when "9"
     exit # this will cause the program to terminate
